eb1fea8862d1156c6e2e3abacf957a292c3c2e33
JosephGethins <josephjgethins@gmail.com>
2025-02-26 18:14:09 +0000

    Correct synatx for the functions

---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+    -- Load your map or other resources here
+    -- Example: mygame/maps/map_name
+end
+
+function love.update(dt)
+    -- Update your game state here
+end
+
+function love.draw()
+    -- Draw your game elements here
+end
